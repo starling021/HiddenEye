53efe7442adbd7bcaf2ede5756f6fb6cff5d6008
Artur Tretiak <stikyt@protonmail.com>
2019-10-02 18:54:03 +0300

    Converted to Class-based structure

--- Defs/Checks.py
+++ Defs/Checks.py
@@ -16,63 +16,29 @@ MAIN0, MAIN1, MAIN2, MAIN3, MAIN4 = colorTheme[0], colorTheme[
 
 
 class ConnectionManager:
-    def verifyNetHunterConnection(self):
+    def verifyNetHunterConnection():
         """Needed to fix problem when HiddenEye under NetHunter don't have internet connection."""
         if(not environ.get('PYTHONHTTPSVERIFY', "") and getattr(ssl,  '_create_unverified_context', None)):
             ssl._create_default_https_context = ssl._create_unverified_context
 
-    def exceptConnection(self, customHost, additionalText):
-        def checkConnection(self, host='https://google.com'):  # Connection     check
-            host = customHost
-            text = additionalText
-            system('clear')
-            try:
-                urlopen(host, timeout=10)
-                print(_("{0}Internet is available.{1}").format(MAIN3, DEFAULT))
-                return True
-            except:
-                return False
-            checkConnection(host)
+    def confirmConnection(host='https://google.com'):
+        system('clear')
+        try:
+            urlopen(host, timeout=10)
+            print("{0}Internet is available.{1}".format(MAIN3, MAIN4))
+            return True
+        except:
+            print('''{1}
+            _  _ . ___  ___  ___ _  _  {0}___ _  _ ___{1}
+            |__| | ]  | ]  | |__ |\ |  {0}|__ \__/ |__{1}
+            |  | | ]__| ]__| |__ | \|  {0}|__  ||  |__{1}
 
-            if checkConnection(self, text) == False:
-                print('''{1}
-                _  _ . ___  ___  ___ _  _  {0}___ _  _ ___{1}
-                |__| | ]  | ]  | |__ |\ |  {0}|__ \__/ |__{1}
-                |  | | ]__| ]__| |__ | \|  {0}|__  ||  |__{1}
-
-                {0}[{1}!{0}]{1} ^Network error^. Verify your Internet connection.''' + '\n' + additionalText).format(MAIN0, MAIN4)
-                exit(0)
-
-
-def checkNgrok():  # Ngrok check
-    if path.isfile('Server/ngrok') == False:  # Is Ngrok downloaded?
-        print('[*] Ngrok Not Found !!')
-        # Question where user must accept education purposes
-        if input("\n{2}[{1}!{2}]{1} Do you accept installation of Ngrok? ({0}y{1}/{2}n{1})\n{2}HiddenEye >>> {0}").format(MAIN2, MAIN4, MAIN0).upper() != 'Y':
-            system('clear')
-            print('\n\n[ {0}YOU ARE NOT AUTHORIZED TO USE THIS TOOL.PLEASE INSTALL NGROK!{1} ]\n\n').format(
-                MAIN0, MAIN4)
+            {0}[{1}!{0}]{1} ^Network error^. Verify your Internet connection.\n'''.format(MAIN0, MAIN4))
             exit(0)
-        else:
-            print('[*] Downloading Ngrok...')
-            if 'Android' in str(check_output(('uname', '-a'))) or 'arm' in str(check_output(('uname', '-a'))):
-                filename = 'ngrok-stable-linux-arm.zip'
-            else:
-                ostype = systemos().lower()
-                if architecture()[0] == '64bit':
-                    filename = 'ngrok-stable-{0}-amd64.zip'.format(ostype)
-                else:
-                    filename = 'ngrok-stable-{0}-386.zip'.format(ostype)
-            url = 'https://bin.equinox.io/c/4VmDzA7iaHb/' + filename
-            download(url)
-            system('unzip ' + filename)
-            system('mv ngrok Server/ngrok')
-            system('rm -Rf ' + filename)
-            system('clear')
 
 
 class PermissionsManager:
-    def checkPermissions(self):
+    def checkPermissions():
         if systemos() == 'Linux':
             if getuid() == 0:
                 print("{0}Permissions granted!".format(MAIN3))
